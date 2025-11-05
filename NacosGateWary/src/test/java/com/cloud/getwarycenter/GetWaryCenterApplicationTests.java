6737abbae3eac5a4358eeaa85277fe814facb18e
张权 <18253626835@163.com>
2025-11-05 16:38:08 +0800

    nacos网关

--- NacosGateWary/src/test/java/com/cloud/getwarycenter/GetWaryCenterApplicationTests.java
+++ NacosGateWary/src/test/java/com/cloud/getwarycenter/GetWaryCenterApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cloud.eurekaserver;
+package com.cloud.getwarycenter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class GetWaryCenterApplicationTests {
 
     @Test
     void contextLoads() {
